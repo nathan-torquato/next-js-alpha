98403a92924fc1ce100d5bb973b05966ea90eb6c
Nathan Torquato <n.torquato.silva@gmail.com>
2021-04-02 13:38:42 -0300

    feat: add Navbar and Footer components

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      Copyright 2021 NinjaList
+    </footer>
+  )
+}
+
+export default Footer
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,20 @@
 import Head from 'next/head'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Index Page</title>
+        <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Navbar />
       <main>
         heeey
       </main>
+      <Footer />
     </div>
   )
 }
