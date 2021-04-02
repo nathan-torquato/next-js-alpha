d19db5c79929be11d0932408126fa6affdaeea9d
Nathan Torquato <n.torquato.silva@gmail.com>
2021-04-02 15:22:10 -0300

    feat: redirect user to home page after 3 seconds when they hit the 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,17 @@
 import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 const NotFound = () => {
+  const router = useRouter()
+  const timeToGoToHomePage = 3000;
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/')
+    }, timeToGoToHomePage);
+  }, [])
+
   return (
     <section className="not-found">
       <h1>Oops...</h1>
