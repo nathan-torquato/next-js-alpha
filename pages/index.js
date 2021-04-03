1cc58bef37f0701af11e5320dac631fd906bc724
Nathan Torquato <n.torquato.silva@gmail.com>
2021-04-02 21:59:13 -0300

    chore: enable webpack 5 and default locale

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       <meta name="keywords" content="ninjas"/>
     </Head>
     <main>
-      <h1 className={styles.title}>Homepage</h1>
+      <h1 className={styles.title}>Homepage (webpack 5)</h1>
       <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
       <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
       <Link href="/ninjas/">
